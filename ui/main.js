ecb9e8de156a27ecbe0ac5cf8f1f6acb1f7e2501
SR <shashankr.blr@gmail.com>
2017-02-12 13:25:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
 var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function(){
-    //Make a request to the counter endpoint
-    
+    //Create a request object to the counter endpoint
+    var request = new XMLHttpRequest();
+   
     //Capture the response and store it in a variable
-    
-    //Render the variable in the correct span:
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString;
-}
+    request.onreadystatechange = function() {
+        if (request.readystate === XMLHttpRequest.DONE){
+            //TAKE ACTION
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    //Make a request
+    request.open('GET', 'http://coolcheetah1.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
